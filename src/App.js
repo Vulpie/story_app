1475fc4c1f6d8f714f8b52399644924cc10e2284
Vulpie <wojtas_f@outlook.com>
2020-08-16 22:01:15 +0200

    feat: add parallax-js and backgrounds

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import './App.css'
+import Parallax from 'parallax-js'
+import Header from './components/Header'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	useEffect(() => {
+		const scene = document.querySelector('.scene')
+		const parallaxInstance = new Parallax(scene, {
+			relativeInput: true,
+		})
+		parallaxInstance.friction(0.2, 0.2)
+	}, [])
+	return (
+		<div className="App">
+			<div data-relative-input="true" id="scene" className="scene">
+				<Header />
+				<div data-depth="0.2">My first Layer!</div>
+				<div data-depth="0.6">My second Layer!</div>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+	return (
+		<div className="header" data-depth="0.2">
+			<h1 className="header__title">Interactive story: Dark Woods</h1>
+		</div>
+	)
+}
+
+export default Header
